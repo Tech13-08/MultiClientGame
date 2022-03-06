42aa0a2c52eba366e4d22b723b29234f80e083c5
Tech13-08 <68032044+Tech13-08@users.noreply.github.com>
2022-03-05 18:47:46 -0800

    Update server.py

--- Multi Client Game/server.py
+++ Multi Client Game/server.py
@@ -1,76 +1,59 @@
 import socket
-import threading
-import pickle
-from game import Game
-
-server = socket.gethostbyname(socket.gethostname())
-port = 5050
+from _thread import *
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+server = '10.1.149.170'
+port = 5050
+
+server_ip = socket.gethostbyname(server)
+
 try:
     s.bind((server, port))
+
 except socket.error as e:
-    str(e)
+    print(str(e))
 
 s.listen(2)
-print("Waiting for a connection, Server Started")
+print("Waiting for a connection")
 
-connected = set()
-games = {}
-idCount = 0
+currentId = "0"
+pos = ["0:50,50", "1:100,100"]
+def threaded_client(conn):
+    global currentId, pos
+    conn.send(str.encode(currentId))
 
-
-
-def threaded_client(conn, p, gameId):
-    global idCount
-    conn.send(str.encode(str(p)))
-
+    currentId = "1"
+    reply = ''
     while True:
         try:
-            data = conn.recv(4096).decode()
-
-            if gameId in games:
-                game = games[gameId]
-
-                if not data:
-                    break
-                else:
-                    if data == "reset":
-                        game.resetWent()
-                    elif data != "get":
-                        game.play(p, data)
-
-                    conn.sendall(pickle.dumps(game))
-            else:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if not data:
+                conn.send(str.encode("Goodbye"))
                 break
-        except Exception:
+            else:
+                #print("Recieved: " + reply)
+                arr = reply.split(":")
+                id = int(arr[0])
+                pos[id] = reply
+
+                if id == 0: nid = 1
+                if id == 1: nid = 0
+
+                reply = pos[nid][:]
+                #print("Sending: " + reply)
+
+            conn.sendall(str.encode(reply))
+        except:
             break
 
-    print("Lost connection")
-    try:
-        del games[gameId]
-        print("Closing Game", gameId)
-    except Exception:
-        pass
-    idCount -= 1
+    print("Connection Closed")
     conn.close()
 
-
 while True:
-    Conn, Addr = s.accept()
-    print("Connected to:", Addr)
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
 
-    idCount += 1
-    P = 0
-    GameId = (idCount - 1) // 2
-    if idCount % 2 == 1:
-        games[GameId] = Game(GameId)
-        print("Creating a new game...")
-    else:
-        games[GameId].ready = True
-        P = 1
-
-    thread = threading.Thread(target=threaded_client, args=(Conn, P, GameId))
-    thread.start()
-
+    start_new_thread(threaded_client, (conn,))

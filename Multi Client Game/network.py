3c1e72c3d7e83e87d585eacdd8c9bbf3a36547a7
Tech13-08 <68032044+Tech13-08@users.noreply.github.com>
2022-03-05 18:47:29 -0800

    Update network.py

--- Multi Client Game/network.py
+++ Multi Client Game/network.py
@@ -1,29 +1,29 @@
 import socket
-import pickle
 
 
 class Network:
+
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = socket.gethostbyname(socket.gethostname())
+        self.host = "10.1.149.170" # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
+                                    # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
+                                    # ipv4 address. This feild will be the same for all your clients.
         self.port = 5050
-        self.addr = (self.server, self.port)
-        self.p = self.connect()
-
-    def getP(self):
-        return self.p
+        self.addr = (self.host, self.port)
+        self.id = self.connect()
 
     def connect(self):
-        try:
-            self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
-        except Exception:
-            pass
+        self.client.connect(self.addr)
+        return self.client.recv(2048).decode()
 
     def send(self, data):
+        """
+        :param data: str
+        :return: str
+        """
         try:
             self.client.send(str.encode(data))
-            return pickle.loads(self.client.recv(2048 * 2))
+            reply = self.client.recv(2048).decode()
+            return reply
         except socket.error as e:
-            print(e)
-
+            return str(e)
